8a22750286dff4f9fe3b06360d219e13f6eab572
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-01-22 11:55:45 -0700

    started Letters constructor

---
+++ letter.js
@@ -0,0 +1,19 @@
+// Constructor function for creating underscores or inserting correctly guessed letters
+const Letters = function() {
+    // A string value to store the underlying character for the letter
+    this.character = '';
+    // A boolean value that stores whether that letter has been guessed yet
+    this.guessed = false;
+    // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+    //this.return = function() {
+
+    //};
+    // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+    //this.guess = function(character) {
+        //if(this.character.includes(character) {
+            //this.guessed = true;
+        //})
+    //};
+}
+
+module.exports = Letters;
