3ac178fc377aa870afd63176c34001c51e7df8e4
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-01-22 22:43:01 -0700

    updated letter.js, started word.js

--- letter.js
+++ letter.js
@@ -1,19 +1,23 @@
 // Constructor function for creating underscores or inserting correctly guessed letters
 const Letters = function() {
     // A string value to store the underlying character for the letter
-    this.character = '';
+    this.character = character;
     // A boolean value that stores whether that letter has been guessed yet
     this.guessed = false;
     // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-    //this.return = function() {
-
-    //};
+    this.returnSymbol = function() {
+            if(this.guessed === true) {
+                return character;
+            } else {
+                return `_`;
+            }
+    };
     // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-    //this.guess = function(character) {
-        //if(this.character.includes(character) {
-            //this.guessed = true;
-        //})
-    //};
+    this.userGuess = function(userInput) {
+        if(userInput === this.character) {
+            this.guessed = true;
+        }
+    };
 }
 
 module.exports = Letters;
