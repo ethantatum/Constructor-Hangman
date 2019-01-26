bf243446014a04e84674fda7740806324874560c
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-01-26 10:51:33 -0700

    updating word.js

--- word.js
+++ word.js
@@ -1,13 +1,17 @@
 const Letters = require('./letter.js');
 
-const Word = function(selectedWord) {
+const Word = function(selectedWord, userInput) {
     // An array of new Letter objects representing the letters of the underlying word
         this.wordLetters = selectedWord.split(' ').map(function(letter){
             return new Letters(letter);
         });
     // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together
-        this.wordString
+        this.wordString = this.wordLetters.join();
     // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
+        this.userGuess = function(userInput) {
+            this.wordLetters.map(this.userGuess(userInput));
+            return
+        }
 }
 
 module.exports = Word;
