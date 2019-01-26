5425c584312a7d921be7a59d809b17abf0ac9e79
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-01-26 14:02:55 -0700

    updated word.js

--- word.js
+++ word.js
@@ -8,7 +8,7 @@ const Word = function(selectedWord, userInput) {
     // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together
         this.wordString = this.wordLetters.join();
     // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
-        this.userGuess = function(userInput) {
+        this.wordGuess = function(userInput) {
             this.wordLetters.map(this.userGuess(userInput));
             return
         }
